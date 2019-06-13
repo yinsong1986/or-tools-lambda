a374d7a92444159f0f7ca463a0b568434eb3c9d2
Yin Song <yinsong@amazon.com>
2019-06-13 16:09:33 +1000

    Update the package script to support the latest or-tools.

--- package.sh
+++ package.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-easy_install3 py3-ortools
+pip3 install ortools
 
 rm -rf dist dist.zip; mkdir dist
 
-find "/opt/python3/lib/python3.6/site-packages" -path "*.egg/*" -not -name "EGG-INFO" -maxdepth 2 -exec cp -r {} ./dist \;
+find "/opt/python3/lib/python3.6/site-packages" -not -name "*dist-info" -not -name "README.txt" -not -name "pip" -not -name "setuptools" -not -name "pkg_resources" -mindepth 1 -maxdepth 1 -exec cp -r {} ./dist \;
 find -not -name 'dist' -not -name 'Dockerfile' -not -name 'package.sh' -mindepth 1 -maxdepth 1 -exec cp -r {} ./dist \;
 
 chmod -R 755 ./dist
